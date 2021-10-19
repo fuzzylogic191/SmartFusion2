fae26fa3ea65ffe8f892b8d8d0299df5cf83905c
Gregory Kuhn <67237034+fuzzylogic191@users.noreply.github.com>
2021-10-19 09:54:05 -0600

    Update UART.C

--- UART.C
+++ UART.C
@@ -8,43 +8,58 @@
 #include "drivers/mss_gpio/mss_gpio.h"
 #include "CMSIS/system_m2sxxx.h"
 #include "drivers/mss_uart/mss_uart.h"
-#define Microsemi_logo \
-"\n\r \
-** ** ******* ****** ***** **** ***** ****** ** ** ******* \n\r \
-* * * * * * * * * * * * * * * * * \n\r \
-* * * * * * ***** * * **** ****** * * * * * \n\r \
-* * * * * * * * * * * * * * * \n\r \
-* * ******* ****** * * **** ***** ****** * * ******* "
+#include <stdio.h>
+
 /* Main function */
 
 
 /*==============================================================================
  * main() function.
  */
+#define RX_BUFF_SIZE 64
 int main()
 {
-	const uint8_t greeting[] = "\n\rWelcome to SmartFusion "
-	"- customizable system-on-chip (cSoC)";
+	uint8_t rx_buff[64];
+	uint8_t rc[10];
+	uint32_t rx_idx  = 0;
+	uint32_t rx_size;
+	uint32_t gpio_pattern;
 
-	/* Initialize and configure UART0. */
-	 MSS_UART_init
-	 (
-	 &g_mss_uart0,
-	 MSS_UART_57600_BAUD,
-	 MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT
-	 );
-	 /* Send the Microsemi Logo over the UART_0 */
+	char data_memory[16] = "Data Memory";
+	char inst_memory[16] = "Inst Memory";
 
-	 MSS_UART_polled_tx_string( &g_mss_uart0, (const uint8_t *)Microsemi_logo);
+	//Initialize Registers
+	MSS_GPIO_config(MSS_GPIO_0, MSS_GPIO_OUTPUT_MODE);
+	MSS_GPIO_config(MSS_GPIO_1, MSS_GPIO_OUTPUT_MODE);
+	MSS_UART_init(&g_mss_uart0,MSS_UART_9600_BAUD,MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT);
+	
+	//=========================================================================
+	
+	while(1){
+	rx_size = MSS_UART_get_rx(&g_mss_uart0, rx_buff, sizeof(rx_buff));
+	if (rx_size > 0){
+		switch(rx_buff[0]){
+		 case 'D':
+			MSS_UART_polled_tx(&g_mss_uart0, data_memory, sizeof(data_memory));
+			break;
+		 case 'I':
+			 MSS_UART_polled_tx(&g_mss_uart0, inst_memory, sizeof(inst_memory));
+			 break;
+		 case 'O':
+			 gpio_pattern = MSS_GPIO_get_outputs();
+			 gpio_pattern ^= 0xFFFFFFFF;
+			 MSS_GPIO_set_outputs( gpio_pattern );
+			 break;
+		 default:
+			 MSS_UART_polled_tx_string(&g_mss_uart0, "Nothing is happening");
 
-	 /* Send greeting message over the UART_0 */
+		}// case statement
 
-	 MSS_UART_polled_tx( &g_mss_uart0, greeting, sizeof(greeting) );
+	  }// if statement
 
-	 while(1){
 
-	 }
-
+	}//while Loop
 	 return(0);
 }
+
 
