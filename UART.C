a5ed16989621910e17eec7d2e18d94c6f792b2f5
Gregory Kuhn <67237034+fuzzylogic191@users.noreply.github.com>
2021-10-20 16:59:13 -0600

    Update UART.C

--- UART.C
+++ UART.C
@@ -10,7 +10,16 @@
 #include "drivers/mss_uart/mss_uart.h"
 #include <stdio.h>
 
-/* Main function */
+/* Function Decelerations */
+int  char_2_int(char);
+void disp_code(int);
+
+void clr_reset();
+void set_reset();
+void pulse_reset(uint8_t);
+void set_clk(uint8_t);
+void clr_clk();
+
 
 
 /*==============================================================================
@@ -22,30 +31,88 @@ int main()
 	uint8_t rx_buff[64];
 	uint32_t rx_idx  = 0;
 	uint32_t rx_size;
-	char Test[10];
-	int  Test1[10];
-	char message[12] = {'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\0'};
-	int i;
-	i = 0;
+	int opcode;
 
 	//Initialize Registers
-
+	MSS_GPIO_init();
 	MSS_UART_init(&g_mss_uart0,MSS_UART_9600_BAUD,MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT);
 
 	//=========================================================================
 	while (1){
 		rx_size = MSS_UART_get_rx(&g_mss_uart0, rx_buff, sizeof(rx_buff));
-		if (rx_size > 0){
-			Test[i] = rx_buff[0];
+		if( rx_size > 0 ){
+		opcode = char_2_int(rx_buff[0]);
+		disp_code(opcode);
+		}
 
-		if (Test[i] == '\r'){
-			MSS_UART_polled_tx(&g_mss_uart0, Test, sizeof(Test));
-			break;
-			}//inner conditional statement
-			i++;
-		}//outer conditional statement
 	}//loop
 
-
 	 return(0);
+}
+
+int char_2_int(char code){
+	int opc;
+	opc = code;
+	return (opc);
+}
+
+void disp_code(int opc){
+	switch (opc){
+	case 48:
+		MSS_UART_polled_tx_string(&g_mss_uart0, "clr_reset ");
+		break;
+	case 49:
+		MSS_UART_polled_tx_string(&g_mss_uart0, "set_reset ");
+		break;
+	case 50:
+		MSS_UART_polled_tx_string(&g_mss_uart0, "pulse_reset ");
+		break;
+	case 51:
+		MSS_UART_polled_tx_string(&g_mss_uart0, "set_clk ");
+		break;
+	case 52:
+		MSS_UART_polled_tx_string(&g_mss_uart0, "clr_clk ");
+		break;
+	default:
+		MSS_UART_polled_tx_string(&g_mss_uart0, "Please enter a valid command ");
+		break;
+	}//int switch
+}
+
+/* If called this function clears the value on the reset pin;
+ * and sets it low/off
+ */
+void clr_reset(){
+
+
+}
+
+/* If called this function turns the reset pin on/pulls it high
+ * for an indeterminate period of time
+ */
+void set_reset(){
+
+}
+
+/* Creates a pulse reset at a constant period
+ * Takes period as an input
+ */
+void pulse_reset(uint8_t period){
+
+}
+
+/*Sets the clock to a determined frequency
+ *
+ */
+void set_clk(uint8_t freq){
+
+
+}
+
+/* This function clears the clock
+ * '/' sets it low
+ */
+void clr_clk(){
+
+
 }
